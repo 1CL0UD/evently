829f2e8952e0a7a68a202e02995eef7041bcb09c
CL_0UD1 <633shalahuddin@gmail.com>
2024-01-18 15:43:13 +0700

    Footer Done

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
+import Image from 'next/image';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="wrapper flex flex-col flex-center flex-between gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Evently Logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>2023 Evently All Rights Reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
